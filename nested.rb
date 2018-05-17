2f8bb667ed0fe41d68636a947869b9ce67e1d4c0
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-17 13:15:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -131,7 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-dennis_new_info = "Assembly"
+
 programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
 programmer_hash
 end
